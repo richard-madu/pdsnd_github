bf9bc5a00809f0f31ebb027b0fc79980e7b528c7
RICHARD <madurichard09@gmail.com>
2022-04-24 17:57:48 +0100

    Added edited the README and .PY files

--- bikeshare.py
+++ bikeshare.py
@@ -180,6 +180,7 @@ def main():
            
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("Thank you for your time")
             break
 if __name__ == "__main__":
 	main()
